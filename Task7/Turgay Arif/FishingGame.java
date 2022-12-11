31b4f45966533d2c09c6b00288956c6060cd65f5
Turgay Arif <landofshadow@gmail.com>
2022-12-11 17:06:40 +0200

    created enum classes and modify the code

--- Task7/Turgay Arif/FishingGame.java
+++ Task7/Turgay Arif/FishingGame.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class FishingGame {
@@ -5,6 +7,7 @@ public class FishingGame {
 
         Scanner sc = new Scanner(System.in);
         Pond pond = new Pond();
+        List<Fish> fishList = new ArrayList<>();
         showMenu();
 
         var input = sc.nextLine();
@@ -13,12 +16,15 @@ public class FishingGame {
                 case "1" -> pond.showCapacity();
                 case "2" -> pond.showState();
                 case "3" -> {
-                    if (Fish.getFishListSize() > 0) {
+                    if (fishList.size() > 0) {
                         System.out.println("Which fish do you want to add?");
-                        Fish.showFishList();
+                        for (int i = 0; i < fishList.size(); i++) {
+                            System.out.println(i + ": " + fishList.get(i));
+                        }
                         var fishToAdd = sc.nextLine();
-                        var fish = Fish.getFishList().get(Integer.parseInt(fishToAdd) - 1);
+                        var fish = fishList.get(Integer.parseInt(fishToAdd));
                         pond.addFish(fish);
+                        fishList.remove(Integer.parseInt(fishToAdd));
                     } else {
                         System.out.println("You need to create a fish!");
                     }
@@ -29,14 +35,18 @@ public class FishingGame {
                     var type = sc.nextLine();
                     System.out.println("How many kg?");
                     var weight = sc.nextLine();
-                    if (type.equalsIgnoreCase("sheat")) {
+                    if (type.equalsIgnoreCase(String.valueOf(FishType.SHEAT))) {
                         System.out.println("How long is its whiskers in cm? ");
                         var length = sc.nextLine();
-                        SheatFish.createSheatFish(Double.parseDouble(weight), Integer.parseInt(length));
+                        SheatFish sheatFish = new SheatFish(Double.parseDouble(weight), Integer.parseInt(length));
+                        System.out.println("You created: " + sheatFish);
+                        fishList.add(sheatFish);
                     } else {
                         System.out.println("What color?");
                         var color = sc.nextLine();
-                        Carp.createCarpFish(Double.parseDouble(weight), color);
+                        Carp carp = new Carp(Double.parseDouble(weight), color);
+                        System.out.println("You created: " + carp);
+                        fishList.add(carp);
                     }
                 }
                 case "6" -> showMenu();
--- Task7/Turgay Arif/SheatFish.java
+++ Task7/Turgay Arif/SheatFish.java
@@ -3,18 +3,12 @@ public class SheatFish extends Fish {
     private int whiskerLength;
 
     public SheatFish(double weight, int whiskerLength) {
-        setWeight(weight);
+        super(weight);
         this.whiskerLength = whiskerLength;
     }
 
     public int getWhiskerLength() {
         return whiskerLength;
-    }
-
-    public static void createSheatFish(double weight, int whiskerLength) {
-        SheatFish sheatFish = new SheatFish(weight, whiskerLength);
-        System.out.println("Successfully created " + sheatFish);
-        Fish.getFishList().add(sheatFish);
     }
 
     @Override
