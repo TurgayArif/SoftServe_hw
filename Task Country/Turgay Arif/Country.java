63525be75cf7586e171511a48af9686a2a1f5e29
Turgay Arif <landofshadow@gmail.com>
2022-12-11 18:10:48 +0200

    created enum class for continents and corrected code

--- Task Country/Turgay Arif/Country.java
+++ Task Country/Turgay Arif/Country.java
@@ -2,29 +2,10 @@ import java.util.Locale;
 
 public class Country {
     private final String name;
-    private String continent;
+    private Continents continent;
     private int population;
     private double area;
 
-    enum Continents {
-        EUROPE("Europe"),
-        ASIA("Asia"),
-        AFRICA("Africa"),
-        AUSTRALIA("Australia"),
-        SOUTH_AMERICA("South America"),
-        NORTH_AMERICA("North America"),
-        ANTARCTICA("Antarctica");
-
-        private final String displayName;
-        Continents(String displayName) {
-            this.displayName = displayName;
-        }
-
-        @Override
-        public String toString() {
-            return this.displayName;
-        }
-    }
 
     public Country(String name, int population) {
         this.name = name;
@@ -35,14 +16,8 @@ public class Country {
         this.population = population;
     }
 
-    public void setContinent(String continent) {
-        for (Continents c : Continents.values()) {
-            if (continent.equalsIgnoreCase(c.displayName)) {
-                this.continent = continent;
-                return;
-            }
-        }
-        System.out.println("You entered a non-existing continent!");
+    public void setContinent(Continents continent) {
+        this.continent = continent;
     }
 
     public void display() {
@@ -68,11 +43,11 @@ public class Country {
         this.area = area;
     }
 
-    public static void compareTwoCountriesByArea(Country c1, Country c2){
-        if(c1.getArea() == 0 || c2.getArea() ==0) {
+    public static void compareTwoCountriesByArea(Country c1, Country c2) {
+        if (c1.getArea() == 0 || c2.getArea() == 0) {
             System.out.println("The area is 0.");
             return;
-        } else if(c1.getArea() > c2.getArea()){
+        } else if (c1.getArea() > c2.getArea()) {
             System.out.printf(String.format(Locale.US, "%s is bigger than %s. %n" +
                     "Area of %s is: %,.2f square km.%n" +
                     "Area of %s is: %,.2f square km.%n", c1.name, c2.name, c1.name, c1.getArea(), c2.name, c2.getArea()));
